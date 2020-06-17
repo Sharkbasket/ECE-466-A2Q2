1114592665b2179d1d8deeaae2a47684b6822ac9
Shaun McGuigan <Shaun@Goldenrod.local>
2020-06-16 20:12:17 -0700

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "systemc.h"
+#include "producer.h"
+#include "consumer.h"
+#include "fifo.h"
+
+int sc_main(int argc, char* argv[])
+{
+    // Channel instantiation
+    sc_clock fast_clock("fast_clock", 10, SC_NS);
+    sc_clock slow_clock("slow_clock", 20, SC_NS);
+    fifo <int> my_fifo("my_fifo", 4);
+    
+    // Module instantiation & interconnection
+    producer my_producer("my_producer");
+    my_producer.clk(fast_clock);
+    my_producer.out(my_fifo);
+    
+    consumer my_consumer("my_consumer");
+    my_consumer.clk(slow_clock);
+    my_consumer.in(my_fifo);
+    
+    sc_start(200, SC_NS);
+    
+    return 0;
+}
